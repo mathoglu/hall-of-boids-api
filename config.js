025c1168339cc30b7654962ce360c099850b4294
Vili Lähtevänoja <vili.lahtevanoja@gmail.com>
2016-06-28 15:02:35 +0300

    Add config and Sequelize dependencies

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "http_port": 3000,
+  'https_port': 8000,
+  database: {
+    'dialect': 'postgres',
+    'name': '',
+    'user': '',
+    'password': ''
+  },
+  'ssl_keyfile': '',
+  'ssl_certfile': ''
+};
